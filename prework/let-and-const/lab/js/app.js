06a9fe98753d3abe1c5e34dbf24f9cb990f1015d
seido <seid783@gmail.com>
2020-11-22 19:54:09 -0800

    its the pre work for let and const

--- prework/let-and-const/lab/js/app.js
+++ prework/let-and-const/lab/js/app.js
@@ -37,7 +37,7 @@ function displayPics(){
   }
   console.log(rando);
   // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the constiable declaration from `const to `let`.
-  // because it was invoked out of the scope.
+  // Because it was invoked out of the scope .
   console.log(viewed);
 
   for (let i = 0; i < 3; i++){
