d73c98e1a55ebdb64af65df00b03208ebbca23ea
Nicholas Carignan <nick.carignan@sbcglobal.net>
2020-12-04 12:25:09 -0800

    starter code

---
+++ class-05/starter-code/server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.static('./public'));
+
+app.listen(PORT, () => console.log('yay the server actually started'));
+
+
